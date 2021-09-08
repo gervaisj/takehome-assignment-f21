3818501112777858e4b4e521bc91b177e27e1a24
gervais <jocelyn.gervais@mail.mcgill.ca>
2021-09-08 09:05:45 -0400

    Update unit tests

--- backend/test_app.py
+++ backend/test_app.py
@@ -1,7 +1,9 @@
-
 # pytest automatically injects fixtures
 # that are defined in conftest.py
 # in this case, client is injected
+import json
+
+
 def test_index(client):
     res = client.get("/")
     assert res.status_code == 200
@@ -14,28 +16,74 @@ def test_mirror(client):
     assert res.json["result"]["name"] == "Tim"
 
 
-def test_get_users(client):
-    res = client.get("/users")
+def test_get_shows(client):
+    res = client.get("/shows")
     assert res.status_code == 200
 
-    res_users = res.json["result"]["users"]
-    assert len(res_users) == 4
-    assert res_users[0]["name"] == "Aria"
+    shows = res.json["result"]["shows"]
+    assert len(shows) == 3
+    assert shows[0]["name"] == "Game of Thrones"
 
 
-def tests_get_users_with_team(client):
-    res = client.get("/users?team=LWB")
+def test_get_shows_filtered(client):
+    res = client.get("/shows?minEpisodes=3")
     assert res.status_code == 200
 
-    res_users = res.json["result"]["users"]
-    assert len(res_users) == 2
-    assert res_users[1]["name"] == "Tim"
+    shows = res.json["result"]["shows"]
+    assert len(shows) == 2
+    assert shows[0]["name"] == "Naruto"
 
 
-def test_get_user_id(client):
-    res = client.get("/users/1")
+def test_get_show_with_id(client):
+    res = client.get("/shows/3")
     assert res.status_code == 200
 
-    res_user = res.json["result"]["user"]
-    assert res_user["name"] == "Aria"
-    assert res_user["age"] == 19
+    res_user = res.json["result"]
+    assert res_user["name"] == "Black Mirror"
+    assert res_user["episodes_seen"] == 3
+
+
+def test_create_show(client):
+    name = 'Breaking Bad'
+    episodes_seen = 34
+    res_success = client.post("/shows",
+                              data=json.dumps({'name': name, 'episodes_seen': episodes_seen}),
+                              content_type='application/json')
+    assert res_success.status_code == 201
+
+    res_user = res_success.json["result"]
+    assert res_user["name"] == name
+    assert res_user["episodes_seen"] == episodes_seen
+
+
+def test_create_show_wrong_type(client):
+    res_fail = client.post("/shows",
+                           data=json.dumps(['a name']),  # wrong type
+                           content_type='application/json')
+    assert res_fail.status_code == 422
+
+
+def test_create_show_missing_param(client):
+    res_fail = client.post("/shows",
+                           data=json.dumps({'name': 'a name'}),
+                           content_type='application/json')
+    assert res_fail.status_code == 422
+
+
+def test_create_show_unexpected_param(client):
+    res_fail = client.post("/shows",
+                           data=json.dumps({'name': 'a name', 'episodes_seen': 1, 'description': ''}),
+                           content_type='application/json')
+    assert res_fail.status_code == 422
+
+
+def test_update_show(client):
+    new_episodes_seen = 4
+    res = client.put("/shows/3",
+                     data=json.dumps({'episodes_seen': new_episodes_seen}),
+                     content_type='application/json')
+    assert res.status_code == 201
+
+    res_user = res.json["result"]
+    assert res_user["name"] == 'Black Mirror'
+    assert res_user["episodes_seen"] == new_episodes_seen
